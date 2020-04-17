f69fca98d6846655ab909ed7c7fd7619e64a041d
Andrey <fox451@yandex.ru>
2020-04-17 17:09:39 +0700

    add image app, add import images form

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '0-rrg*ybfgh4_+9z!0@r_zt0^zb+m&r0%!_^vp&6u-ie4edzg#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
